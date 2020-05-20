f9a229c3b7e7cb678adea95c36950ba0998337a0
shadow-wolfy001 <65371530+shadow-wolfy001@users.noreply.github.com>
2020-05-21 00:47:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// other commands...
+});
+
+client.login(token);
+
