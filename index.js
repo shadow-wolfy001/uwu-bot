5b44cc6674ec20b4d096c04583cedb7e2dd154bb
shadow-wolfy001 <65371530+shadow-wolfy001@users.noreply.github.com>
2020-06-14 23:15:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,59 +5,33 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === '*channel') {
-    msg.reply(' please consider subscribing to the youtube channel :) https://www.youtube.com/channel/UCf-x69JKyUepbgtTuDtYJ1w ');
-  }
-});
 
-client.login('Token goes here');
-
-client.on('message', msg => {
-  if (msg.content === '*serverinvite') {
-    msg.channel.send('https://discord.gg/ruVjkd');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '<@704580626774491187>') {
-    msg.channel.send('My prefix is * :slight_smile:');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '*github') {
-    msg.reply('https://github.com/shadow-wolfy001/AZGbot')
-  }
-});
-
-
+client.login(token);
 
 
 
 client.on('guildMemberAdd', member =>{
   let welcomeEmbed = new Discord.MessageEmbed()
   .setColor('#63e764')
-  .setTitle('**Welcome to AZG clan!**')
-  .addField("**New Member:**", `Welcome ${member} to the AZG clan! please enjoy your time here :) Make sure to read <#719751812504354818> If you have any issues please contact a moderator, Thanks :slight_smile:`, false)
+  .setTitle('**Welcome to Aviation hangouts!**')
+  .addField("**New Member:**", `Welcome ${member} to the Aviation Hangouts Discord Server! please enjoy your time here :) Make sure to read <#718914777291948113> If you have any issues please contact a member of staff, Thanks :slight_smile:`, false)
   .setTimestamp()
-      member.guild.channels.cache.get('719736897806663743').send(welcomeEmbed)
+      member.guild.channels.cache.get('718914777291948113').send(welcomeEmbed)
 });
 
-client.on('message', msg => {
-  if (msg.content === '*help') {
-    msg.reply('My current comands are *channel (shows you Aliens youtube)  *invites (shows the amount of invites you have), *report (to report a bug or a member), *serverinvite (shows the invite for this server incase you want to invite a friend), ban and kick (mods only) and the say command, *github (shows bots github page). more commands soon though :wink:')
-  }
-  });
+
 
   client.on('message', msg => {
-    if (msg.content === '*version') {
-        msg.reply('I am currently in version 1.00 :slight_smile:')
+    if(msg.content === '!version') {
+        msg.reply('I am currently in version 1.50 :slight_smile:')
   }
 });
+
+
+
   
   client.on('message', msg => {
-  if (msg.content === '*invites') {
+  if (msg.content === '!invites') {
 
     var userId = msg.author.id;
 
@@ -79,7 +53,7 @@ client.on('message', msg => {
 
 
   client.on('message', msg => {
-    if (msg.content === '*report') {
+    if (msg.content === '!report') {
       msg.channel.send('To report an issue with the bot, such as its offline or isnt respnding, please DM Shadow Wolfy#8860, for any reports against a users behaviour please DM any available moderators :slight_smile:');
     }
   });
@@ -98,13 +72,13 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
   
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
+  client.user.setActivity(`I am currently stalking ${client.guilds.cache.size} servers`);
 });
 
 client.on("guildDelete", guild => {
 
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
+  client.user.setActivity(`I am currently stalking ${client.guilds.cache.size} servers`);
 });
 
 
@@ -128,8 +102,8 @@ client.on("message", async message => {
   }
   
   if(command === "say") {
-    if(!message.member.roles.cache.some(r=>["Moderator"].includes(r.name)))
-    return message.reply("Sorry, you don't have permissions to use this!");
+    if(!message.member.roles.cache.some(r=>["Staff"].includes(r.name)))
+    return message.reply("Lol noob, you dont have permission");
    
     const sayMessage = args.join(" ");
     
@@ -137,18 +111,17 @@ client.on("message", async message => {
     
     message.channel.send(sayMessage);
   }
-  
   if(command === "kick") {
     
-    if(!message.member.roles.cache.some(r=>["Moderator"].includes(r.name)))
-      return message.reply("You dont have permission to do this :rolling_eyes");
+    if(!message.member.roles.cache.some(r=>["Staff"].includes(r.name)))
+      return message.reply("You dont have permission to do this :rolling_eyes:");
     
     
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
       return message.reply("Please mention a valid member of this server");
     if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      return message.reply("Uh Oh, missing permissions, please give me permissions to perform this action");
     
     
     let reason = args.slice(1).join(' ');
@@ -163,14 +136,14 @@ client.on("message", async message => {
   
   if(command === "ban") {
     
-    if(!message.member.roles.cache.some(r=>["Moderator"].includes(r.name)))
-      return message.reply("You dont have permission to do this :rolling_eyes");
+    if(!message.member.roles.cache.some(r=>["Staff"].includes(r.name)))
+      return message.reply("You dont have permission to do this :rolling_eyes:");
     
     let member = message.mentions.members.first();
     if(!member)
       return message.reply("Please mention a valid member of this server");
     if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+      return message.reply("Uh Oh, missing permissions, please give me permissions to perform this action");
 
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "No reason provided";
@@ -180,5 +153,7 @@ client.on("message", async message => {
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
 }); 
+
+
 
 
